d6fbcd0dc366da8df7f38539ae419dd463578257
Rafael Hernández Larios <rafah.larios@gmail.com>
2019-03-04 23:25:26 -0600

    :gift: add string cleaner functions

--- src/common.js
+++ src/common.js
@@ -16,47 +16,75 @@ const urlReGex = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\
 * this function evaluate a string to know if it 's an url
 */
 function isURL(string) {
-    const res = new RegExp(urlReGex);
-    return res.test(string);
+  const res = new RegExp(urlReGex);
+  return res.test(string);
 }
 
 async function cleanFields(Item) {
-    let param_names = Object.keys(Item);
-    let subObject;
-    param_names.forEach((item) => {
-      if (typeof Item[item] === 'object') {
-        if (isEmpty(Item[item])) delete Item[item];
-        else {
-          subObject = Object.keys(Item[item]);
-          subObject.forEach((subitem) => {
-            if (!notUndefined(Item[item][subitem]) || isEmpty(Item[item][subitem]))
-              delete Item[item][subitem];
-          });
-        }
-      } else {
-        if (!notUndefined(Item[item]))
-          delete Item[item];
+  let param_names = Object.keys(Item);
+  let subObject;
+  param_names.forEach((item) => {
+    if (typeof Item[item] === 'object') {
+      if (isEmpty(Item[item])) delete Item[item];
+      else {
+        subObject = Object.keys(Item[item]);
+        subObject.forEach((subitem) => {
+          if (!notUndefined(Item[item][subitem]) || isEmpty(Item[item][subitem]))
+            delete Item[item][subitem];
+        });
       }
-    });
-    return Item;
-  }
-  
-  function notUndefined(val) {
-    return (val !== null && val !== undefined && val !== "");
-  }
-
-  function isEmpty(obj) {
-    for(var key in obj) {
-      if(obj.hasOwnProperty(key))
-        return false;
+    } else {
+      if (!notUndefined(Item[item]))
+        delete Item[item];
     }
-    return true;
+  });
+  return Item;
+}
+  
+function notUndefined(val) {
+  return (val !== null && val !== undefined && val !== "");
+}
+
+function isEmpty(obj) {
+  for(var key in obj) {
+    if(obj.hasOwnProperty(key))
+      return false;
   }
+  return true;
+}
+
+async function cleanedObject(obj) {
+  try {
+    for (let item in obj) {
+      obj[item] = await getCleanedString(obj[item])
+    }
+    return obj;
+  } catch (err) {
+    throw (err);
+  }
+}
+
+async function getCleanedString(st) {
+  try {
+    st = st.toLowerCase();
+    st = st.replace(/á/gi, "a");
+    st = st.replace(/é/gi, "e");
+    st = st.replace(/í/gi, "i");
+    st = st.replace(/ó/gi, "o");
+    st = st.replace(/ú/gi, "u");
+    st = st.replace(/ñ/gi, "n");
+    return st;
+  } catch (err) {
+    throw (err);
+  }
+}
 
 module.exports = {
-    isURL,
-    uuidRegex,
-    cleanFields,
-    notUndefined,
-    isEmpty
+  isURL,
+  uuidRegex,
+  cleanFields,
+  notUndefined,
+  isEmpty,
+  getCleanedString,
+  cleanedObject
 }
